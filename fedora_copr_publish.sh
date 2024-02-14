1939d0c6a7416629261a115f4007053ec9b34eee
Marcin Szydelski <marcin@szydelscy.pl>
2024-02-14 15:21:02 +0100

    get version from Cargo.toml

--- fedora_copr_publish.sh
+++ fedora_copr_publish.sh
@@ -1,16 +1,23 @@
 #!/usr/bin/env bash
 #
 # Parse debian/changelog and set package version same as on PopOS! and Ubuntu
-# 
+#
 # ----------------------------------------------------------------------------
+# 2024-02-14 Marcin Szydelski
+#  get version from Cargo.toml
+#  fix shellcheck issues
 # 2021-01-09 Marcin Szydelski
-#		init
+#  init
 
 # config
-export outdir="$(pwd)/.rpkg-build"
+outdir="$(pwd)/.rpkg-build"
+export outdir
 
 # verification
-[ -f debian/changelog ] || { echo "No debian/changelog found."; exit 1; }
+[ -f Cargo.toml ] || {
+  echo "No Cargo.toml configuration found."
+  exit 1
+}
 
 # fetch upstream
 git fetch upstream
@@ -18,26 +25,22 @@ git fetch upstream
 git checkout master
 git merge upstream/master -m "fetch upstream" --log
 
+[ -d "${outdir}" ] && rm -rf "${outdir}"
 
-[ -d ${outdir} ] && rm -rf ${outdir}
-
-version_in_changelog=$(grep -E "firmware-manager \([[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+" debian/changelog | head -1)
-_tmp=${version_in_changelog%%)*}
-_tmp=${_tmp%%~*}
-version=${_tmp##*\(}
+version=$(python3 -c 'import toml;config = toml.load("Cargo.toml");print(config["package"]["version"])')
 
 _tmp=$(git tag --list firmware-manager-"$version"-'*' | sort -n -t '-' -k 4 -r | head -1)
 release=${_tmp##*-}
 
 if [ "z$release" == "z" ]; then
-	release=1
+  release=1
 else
-	if ! [[ "$release" =~ ^[0-9]+$ ]]; then
-		echo "Release should be a number"
-		exit 2
-	fi
-	# increment release number
-	((release++))
+  if ! [[ "$release" =~ ^[0-9]+$ ]]; then
+    echo "Release should be a number"
+    exit 2
+  fi
+  # increment release number
+  ((release++))
 fi
 
 # as a workaround set static version in spec file
@@ -47,23 +50,35 @@ git commit -m"bump Version to: $version-$release" firmware-manager.spec.rpkg
 
 #test & build srpm
 mkdir "$outdir"
-rpkg local --outdir="$outdir" || { echo "rpkg local failed"; exit 4; }
+rpkg local --outdir="$outdir" || {
+  echo "rpkg local failed"
+  exit 4
+}
 
 # rpkg tag
-rpkg tag --version="$version"  --release="$release"
+rpkg tag --version="$version" --release="$release"
 
 srpm="$(ls .rpkg-build/firmware-manager-*.src.rpm)"
 
 # publish / build oficially
 
-copr-cli build system76 "$srpm" || { echo "Copr build failed"; exit 5; }
+copr-cli build system76 "$srpm" || {
+  echo "Copr build failed"
+  exit 5
+}
 
 # store in repo
-git push || { echo "Git push failed"; exit 6; }
-git push --tags || { echo "Git push --tags failed"; exit 6; }
+git push || {
+  echo "Git push failed"
+  exit 6
+}
+git push --tags || {
+  echo "Git push --tags failed"
+  exit 6
+}
 
 # clear
 
 if [ -d "$outdir" ]; then
-	rm -rf "$outdir"
+  rm -rf "$outdir"
 fi
